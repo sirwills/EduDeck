ff2781d0cd742cd3d68c66f69e69d4c7a58da40c
Odali Wills <odaliwills@gmail.com>
2023-08-23 22:40:30 +0100

    created a dashboard component for the user profile

--- frontend/src/Components/userProfile/UserProfile.jsx
+++ frontend/src/Components/userProfile/UserProfile.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Dashboard from '../dashboard/Dashboard'
 
 const UserProfile = () => {
   return (
-    <div>UserProfile</div>
+    <main className='flex w-screen h-screen'>
+        <aside className='flex basis-3/12 w-[10%vw] bg-green-600'>
+
+        </aside>
+        
+
+        <section className='flex basis-9/12 w-64'>
+
+            <Routes>
+                <Route path='/dashboard' element={<Dashboard/>}/>
+            </Routes>
+
+        </section>
+    </main>
   )
 }
 
